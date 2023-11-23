9e6ffe66c4bb87df577159d158cfceb8f6d600ab
Alexander03e <cfifeg1@gmail.com>
2023-11-23 15:36:12 +0300

    'fixstyles'

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ fetch('https://rest.info-medika.ru:45678/GET_pl_exam_grp')
   // Create table body rows for each ID in the JSON data
   const tbody = document.createElement('tbody');
   data.forEach(item => {
-    const row = document.createElement('tr');
+    const row = document.createElement('p');
     row.classList.add('row-table1')
     // Create table cell for the ID property in the JSON data
     const idCell = document.createElement('td');
@@ -19,11 +19,15 @@ fetch('https://rest.info-medika.ru:45678/GET_pl_exam_grp')
     idCell.appendChild(document.createTextNode(item.pl_ex_gr_id));
 
     // Create table cell for the name property in the JSON data
-    const nameCell = document.createElement('td');
+    const nameCell = document.createElement('p');
 
     // Create a button element
     const button = document.createElement('button');
     button.classList.add('button-table1')
+    button.classList.add('nav-links')
+    button.classList.add('nav-lines')
+    button.classList.add('btn')
+    button.classList.add('table__btn')
     button.innerText = item.name; // Set the button text to the name property
     // Add a click event listener to the button
     
@@ -55,7 +59,7 @@ fetch('https://rest.info-medika.ru:45678/GET_pl_exam_grp')
         const modifiedNames = []; // Array to store unique modified names
         uniqueData.forEach(secondItem => {
           
-          const secondRow = document.createElement('tr');
+          const secondRow = document.createElement('p');
           secondRow.classList.add('row-table2')
           // Remove "первичный" and "повторный" from the name property
           const modifiedName = secondItem.name.replace(/первичный|повторный/g, '');
@@ -66,11 +70,14 @@ fetch('https://rest.info-medika.ru:45678/GET_pl_exam_grp')
             modifiedNames.push(modifiedName); // Add modified name to the array
     
             // Create table cell for the modified name property
-            const secondNameCell = document.createElement('td');
+            const secondNameCell = document.createElement('p');
             
             const innerButton = document.createElement("button");
             innerButton.classList.add('button-table2')
-            
+            innerButton.classList.add('nav-links')
+            innerButton.classList.add('nav-lines')
+            innerButton.classList.add('btn')
+            innerButton.classList.add('table__btn')
             innerButton.innerText = modifiedName;
             secondNameCell.appendChild(innerButton);
     
@@ -81,14 +88,9 @@ fetch('https://rest.info-medika.ru:45678/GET_pl_exam_grp')
             
             let innerButtonsall = document.querySelectorAll('.button-table2')
             innerButton.addEventListener('click', function() {
-              innerButton.classList.add('-active')
-              innerButtonsall.forEach(button => {
-                console.log('ee')
-                button.classList.remove('-active')
-              })
-              button.classList.add('-active')
+              
               if (document.querySelector('.Registr_Table2').innerHTML!=' '){
-              document.querySelector('.Registr_Table3').innerHTML = '<button>Первичный</button><br><button>Повторный</button>'
+              document.querySelector('.Registr_Table3').innerHTML = '<button class="nav-links nav-lines table__btn btn">Первичный</button><br><button class="nav-links nav-lines table__btn btn">Повторный</button>'
               } else{
                 document.querySelector('.Registr_Table3').innerHTML= ' '
               }
